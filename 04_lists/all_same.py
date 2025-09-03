83f2c3871d17bc3980df3da7eb2891279396f2bc
Elijah Myers <emyers9259@floridapoly.edu>
2025-09-03 17:42:58 +0000

    04_lists

---
+++ 04_lists/all_same.py
@@ -0,0 +1,17 @@
+n = int(input("Enter the number of elements"))
+
+elements = []
+while n:
+    elements.append(int(input()))
+    n -= 1
+
+print(elements)
+
+# Check if all the elements are equal
+# print true
+# otherwise false
+if len(elements) < 2:
+    print(bool(True))
+else:
+    print(bool(False))
+
